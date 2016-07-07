fd06c253b665c533b13472a47a5bab14cca90bfd
MaxKrissigo <maxkortge@yahoo.com>
2016-07-07 14:50:22 +1000

    Delete animate.js

--- animate.js
+++
@@ -1,7 +0,0 @@
-$("div img").bind("webkitAnimationEnd animationEnd", function(){
-  $(this).removeClass("animated")  
-})
-
-$("div img").hover(function(){
-  $(this).addClass("animated");        
-})
